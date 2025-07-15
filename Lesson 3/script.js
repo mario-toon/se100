0e51f80c96957b8c7085118d44935fa5e8a63f6c
mario-toon <mario.toon@gmail.com>
2025-07-15 22:02:59 +0800

    Arranged Lesson 2 + created Lesson 3

---
+++ Lesson 3/script.js
@@ -0,0 +1,8 @@
+console.log(4 < 5); //returns true 
+console.log(4 > 5); //returns false
+console.log(5 <= 5) //returns true
+console.log(5 >= 5) //returns true
+console.log("5" == 5) //returns true
+console.log("5" === 5) //returns false
+console.log(5 != 4) //return true
+console.log(5 !== "4") //return true
